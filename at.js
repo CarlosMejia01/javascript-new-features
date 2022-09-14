44dbca178b43622f982641d370bc75d4692eaf2c
Carlos Mejia <cmejia@equitel.com.co>
2022-09-13 22:22:08 -0500

    at js

---
+++ at.js
@@ -0,0 +1,5 @@
+const x = [11, 22, 44, 55, 66, 77, 88, 99];
+
+console.log(x.at(-1));
+console.log(x.at(-3));
+console.log(x.at(-5));
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 try {
-	const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+	const response = await fetch("https://jsonplaceholder.typicode.com/postsa");
 	const data = await response.json();
 	console.log(data);
 } catch (err) {
